11e4adef6aa9796bed47daab5c850f7f2c80566d
Blake Embrey <hello@blakeembrey.com>
2016-10-06 16:38:33 -0700

    Allow `null` and `undefined` values to `xtend`

--- immutable.d.ts
+++ immutable.d.ts
@@ -1,7 +1,7 @@
-declare function xtend <A> (a: A): A;
-declare function xtend <A, B> (a: A, b: B): A & B;
-declare function xtend <A, B, C> (a: A, b: B, c: C): A & B & C;
-declare function xtend <A, B, C, D> (a: A, b: B, c: C, d: D): A & B & C & D;
-declare function xtend <A, B, C, D, E> (a: A, b: B, c: C, d: D, e: E): A & B & C & D & E;
+declare function xtend <A> (a: A | null | undefined): A;
+declare function xtend <A, B> (a: A | null | undefined, b: B | null | undefined): A & B;
+declare function xtend <A, B, C> (a: A | null | undefined, b: B | null | undefined, c: C | null | undefined): A & B & C;
+declare function xtend <A, B, C, D> (a: A | null | undefined, b: B | null | undefined, c: C | null | undefined, d: D | null | undefined): A & B & C & D;
+declare function xtend <A, B, C, D, E> (a: A | null | undefined, b: B | null | undefined, c: C | null | undefined, d: D | null | undefined, e: E | null | undefined): A & B & C & D & E;
 
 export = xtend;
